28f3f96fda505f81d4847f2aeb0ef20c3de2fe6a
GlaDOS-PC\GlaDOS <vicmeakin@gmail.com>
2017-03-27 21:08:37 +0100

    Assets, plus basic gui added

---
+++ BowlMaster/Assets/_Scripts/Ball.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float ThrowForce = 50;
+
+    private AudioSource mySound;
+    private bool ballIsRolling;
+    private Rigidbody myBody;
+
+	// Use this for initialization
+	void Start () {
+        mySound = GetComponent<AudioSource>();
+	    myBody = GetComponent<Rigidbody>();
+	    ballIsRolling = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+	    if (ballIsRolling)
+	    {
+	        MoveBall();
+	    } else if (Input.GetMouseButtonDown(0)) //ball is not rolling, so see if they have clicked to roll it
+        {
+            RollBall();
+        }
+        */
+
+	    if (!ballIsRolling)
+	    {
+	        RollBall();
+	    }
+    }
+
+    private void MoveBall()
+    {
+        //throw new NotImplementedException();
+    }
+
+
+    public void RollBall()
+    {
+        myBody.AddForce(transform.forward*ThrowForce,ForceMode.Impulse);
+        mySound.Play();  //removed, sound will start when ball hits the floor, see on collision enter method below //put back in again as I don't like the affect! might need to change it so different sound effects depending on how it's triggered eg just dropped vs thrown by this method
+        ballIsRolling = true;
+    }
+    /*
+    //TODO - might need to change it so different sound effects depending on how it's triggered eg just dropped vs thrown by this method, as currently this triggers if dropped but gives the rolling affect
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("1");
+            if (collision.collider.gameObject.tag == "Lane")
+            {
+                Debug.Log("2");
+                mySound.Play();
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.collider.gameObject.tag == "Lane")
+            {
+                mySound.Stop();
+            }
+        }
+    */
+}
---
+++ BowlMaster/Assets/_Scripts/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float cameraZStop = 1700f;
+    private Vector3 offset;
+    private Ball ball;
+
+    // Use this for initialization
+    void Start ()
+	{
+	    //ball = GameObject.Find("Ball");
+	    ball = GameObject.FindObjectOfType<Ball>();
+	    if (!ball)
+	    {
+	        Debug.LogError("No ball");
+	    }
+
+	    offset = transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        //camera should follow ball until almost at the pins, then stop
+	    if (transform.position.z <= cameraZStop)
+	    {
+	        transform.position = ball.transform.position + offset;
+	    }
+	}
+}
